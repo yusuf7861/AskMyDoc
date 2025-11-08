7743e737298ac8fea8fe3c5ff69f24715f6cebdc
Yusuf Jamal <72370431+yusuf7861@users.noreply.github.com>
2025-11-08 10:54:57 +0530

    Initialize Maven project with wrapper scripts, configuration files, and basic application structure

---
+++ src/main/java/com/askmydoc/askmydoc/AskMyDocApplication.java
@@ -0,0 +1,13 @@
+package com.askmydoc.askmydoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMyDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskMyDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/askmydoc/askmydoc/AskMyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askmydoc.askmydoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskMyDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
